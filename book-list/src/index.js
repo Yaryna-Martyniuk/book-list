0657897e81f3214ad3adc5a6f9951b3136ff149a
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-03-29 20:15:08 +0300

    inline styles for author title added

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./index.css"
-
+import './index.css';
 
 const App = () => {
     return (
@@ -32,7 +31,13 @@ const Image = () => (
 );
 const Title = () => <h2>Тигролови (Шкільна полиця)</h2>;
 const Author = () => {
-    return <h4>Іван Багряний</h4>;
+    const inlineHeadingStyles = {
+        color: '#617d98',
+        fontSize: '0.75rem',
+        marginTop: '0.5rem',
+    };
+
+    return <h4 style={inlineHeadingStyles}>Іван Багряний</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
