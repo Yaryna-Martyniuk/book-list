b3506d415fdeead94672e8fd597d6fd36dad558f
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-03-29 20:49:30 +0300

    other book added

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -2,42 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const firstBook = {
+    author: 'Іван Багряний',
+    title: 'Тигролови (Шкільна полиця)',
+    img: 'https://book-ye.com.ua/upload/resize_cache/iblock/27b/520_860_1/52eea9f5_914c_11e6_80c0_000c29ae1566_b1dd01be_eaf3_11ec_8171_0050568ef5e6.jpg',
+};
+
+const secondBook = {
+    author: 'Михайло Коцюбинський',
+    title: 'Тіни забутих предків',
+    img: 'https://book-ye.com.ua/upload/resize_cache/iblock/7e5/520_860_1/83bcc485_c7d5_11ed_8182_00505684ea69_187adeff_c7d7_11ed_8182_00505684ea69.jpg',
+};
+
 const App = () => {
     return (
         <section className='booklist'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book
+                author={firstBook.author}
+                title={firstBook.title}
+                img={firstBook.img}
+            />
+            <Book
+                author={secondBook.author}
+                title={secondBook.title}
+                img={secondBook.img}
+            />
         </section>
     );
 };
 
-const Book = () => {
+const Book = (props) => {
+    console.log(props)
     return (
         <article className='book'>
-            <Image />
-            <Title />
-            <Author />
+            <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <h4>{props.author}</h4>
         </article>
     );
-};
-
-const Image = () => (
-    <img
-        src='https://book-ye.com.ua/upload/resize_cache/iblock/27b/520_860_1/52eea9f5_914c_11e6_80c0_000c29ae1566_b1dd01be_eaf3_11ec_8171_0050568ef5e6.jpg'
-        alt='Тигролови (Шкільна полиця)'
-    />
-);
-const Title = () => <h2>Тигролови (Шкільна полиця)</h2>;
-const Author = () => {
-    const inlineHeadingStyles = {
-        color: '#617d98',
-        fontSize: '0.75rem',
-        marginTop: '0.5rem',
-    };
-
-    return <h4 style={inlineHeadingStyles}>Іван Багряний</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
