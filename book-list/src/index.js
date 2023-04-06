d83e0de06a51be3f6639d70b2efeb4d31f31a58a
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-04-06 15:24:31 +0300

    booklist edited

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -2,42 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-    author: 'Іван Багряний',
-    title: 'Тигролови (Шкільна полиця)',
-    img: 'https://book-ye.com.ua/upload/resize_cache/iblock/27b/520_860_1/52eea9f5_914c_11e6_80c0_000c29ae1566_b1dd01be_eaf3_11ec_8171_0050568ef5e6.jpg',
-};
+const books = [
+    {
+        author: 'Іван Багряний',
+        title: 'Тигролови (Шкільна полиця)',
+        img: 'https://book-ye.com.ua/upload/resize_cache/iblock/27b/520_860_1/52eea9f5_914c_11e6_80c0_000c29ae1566_b1dd01be_eaf3_11ec_8171_0050568ef5e6.jpg',
+        id: 1,
+    },
 
-const secondBook = {
-    author: 'Михайло Коцюбинський',
-    title: 'Тіни забутих предків',
-    img: 'https://book-ye.com.ua/upload/resize_cache/iblock/7e5/520_860_1/83bcc485_c7d5_11ed_8182_00505684ea69_187adeff_c7d7_11ed_8182_00505684ea69.jpg',
-};
+    {
+        author: 'Михайло Коцюбинський',
+        title: 'Тіни забутих предків',
+        img: 'https://book-ye.com.ua/upload/resize_cache/iblock/7e5/520_860_1/83bcc485_c7d5_11ed_8182_00505684ea69_187adeff_c7d7_11ed_8182_00505684ea69.jpg',
+        id: 2,
+    },
+    {
+        author: 'Василь Барка',
+        title: 'Жовтий князь',
+        img: 'https://book-ye.com.ua/upload/iblock/fd6/32994837_85f2_11ed_8177_0050568ef5e6_4fab1548_85f3_11ed_8177_0050568ef5e6.jpg',
+        id: 3,
+    },
+];
 
 const App = () => {
     return (
         <section className='booklist'>
-            <Book
-                author={firstBook.author}
-                title={firstBook.title}
-                img={firstBook.img}
-            />
-            <Book
-                author={secondBook.author}
-                title={secondBook.title}
-                img={secondBook.img}
-            />
+            {books.map((book) => {
+                const { img, title, author, id } = book;
+                return <Book img={img} title={title} author={author} key={id} />;
+            })}
         </section>
     );
 };
 
 const Book = (props) => {
-    console.log(props)
+    const { img, title, author } = props;
+    console.log(props);
     return (
         <article className='book'>
-            <img src={props.img} alt={props.title} />
-            <h2>{props.title}</h2>
-            <h4>{props.author}</h4>
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     );
 };
